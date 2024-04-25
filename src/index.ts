f8c7371fc062e44c9dd010bd0e8beec9de70d6da
Shinichi Takagi <nextplanehome1988@yahoo.co.jp>
2024-04-25 14:24:14 +0300

    Enables CORS

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const insurance2: Insurance = { insuranceNumber: "456", dateOfAccident: "2023-12
 const insurances = [
     insurance1, insurance2
 ];
-/*
+
 const whitelist = ['http://localhost:3000', 'https://insurance-partner.net']; 
 
 const corsOptions: CorsOptions = {
@@ -24,34 +24,19 @@ const corsOptions: CorsOptions = {
       }
     }
 }
-*/
+
 app.use(express.json());
 
-//app.use(cors(corsOptions));
-app.use((req: Request, res: Response, next: any) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-}); 
-
-app.use((req: any, res: Response, next: any) => {
-    if (!req.headers['user-agent'].includes('ELB-HealthChecker')) {
-        console.log('The content of received headers:', req.headers);
-    }
-    next();
-});
-
+app.use(cors(corsOptions));
 
 const apiRouter = express.Router();
 
 app.use('/api', apiRouter);
 
-//app.get('/', (req: Request, res: Response) => {
 apiRouter.get('/', (req: Request, res: Response) => {
-    res.send('Greeting from insurance-api! (CI/CD version)');
+    res.send('Greeting from insurance-api! (CI/CD version, CORS)');
 });
 
-//app.post('/find-insurance', (req: Request, res: Response) => {
 apiRouter.post('/find-insurance', (req: Request, res: Response) => {
     console.log("req: ", req);
     const { dateOfAccident, ssn } = req.body;
@@ -64,7 +49,5 @@ apiRouter.post('/find-insurance', (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-    //console.log(`API server listening at http://localhost:${port}`);
-    //console.log("Waiting for a request...");
-    console.log("HTTP header inspecting version running...");
+
 });
